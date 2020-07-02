1c1696390b41b73a304d30256a6f4fc1618e8a92
Stefano Pesenti <stefano.pesenti@gmail.com>
2020-07-02 15:07:06 +0100

    end of video 93

--- Source/BuildingEscape/WorldPosition.cpp
+++ Source/BuildingEscape/WorldPosition.cpp
@@ -2,6 +2,7 @@
 
 
 #include "WorldPosition.h"
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UWorldPosition::UWorldPosition()
@@ -19,9 +20,9 @@ void UWorldPosition::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Warning, TEXT("This is a warning!"));
-	UE_LOG(LogTemp, Error, TEXT("This is an error!"));
-	UE_LOG(LogTemp, Display, TEXT("This is a display!"));
+	AActor* Owner = GetOwner();
+	FString Name = Owner->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("this component is attached to %s"), *Name);
 	
 }
 
